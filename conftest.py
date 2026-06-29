331a0684341acad378de2088866506ce6c4022ab
Aayushpatel51 <64529257+Aayushpatel51@users.noreply.github.com>
2026-06-29 14:56:05 +0530

    Change Chrome options to start maximized
    Updated Chrome options for the Selenium driver fixture.

--- conftest.py
+++ conftest.py
@@ -5,15 +5,7 @@ from selenium import webdriver
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-
-    # Headless mode
-    options.add_argument("--headless=new")
-
-    # Recommended options for CI/Docker
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(options=options)
 
